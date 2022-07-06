2e4f384dc31baba6d9b866a5f1b87e3af3f64eef
Florian Meinicke <florian.meinicke@cetoni.de>
2022-07-06 15:58:03 +0200

    Bump version to v1.1.0

--- sila_cetoni/core/sila/core_service/server.py
+++ sila_cetoni/core/sila/core_service/server.py
@@ -13,7 +13,7 @@ from .generated.systemstatusprovider import SystemStatusProviderFeature
 logger = logging.getLogger(__name__)
 
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 
 class Server(SilaServer):
