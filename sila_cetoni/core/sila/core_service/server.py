ed090dd1c6bf12041302e7f861c776615c7592ad
Florian Meinicke <florian.meinicke@cetoni.de>
2022-08-02 15:21:47 +0200

    Update CHANGELOG.md and bump version to v1.5.0

--- sila_cetoni/core/sila/core_service/server.py
+++ sila_cetoni/core/sila/core_service/server.py
@@ -13,7 +13,7 @@ from .generated.systemstatusprovider import SystemStatusProviderFeature
 logger = logging.getLogger(__name__)
 
 
-__version__ = "1.4.0"
+__version__ = "1.5.0"
 
 
 class Server(SilaServer):
